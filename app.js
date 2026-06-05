4e728ba3238baaf5c9922f2d9705155308f761f0
kburke131 <karlburke537@gmail.com>
2026-06-05 12:04:59 +0100

    nearly there

--- app.js
+++ app.js
@@ -13,25 +13,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.get('/', (req, res) => {
 
-  res.render("index")
+  res.render("index", {
+    pageTitle: "Burke Passive Fire Protection",
+    pageDesc: "Ireland's Leading Fire Protection Specialists",
+    canonicalPath: "/"
+  })
 
 });
 
 app.get('/services', (req, res) => {
 
-  res.render("services")
+  res.render("services", {
+    pageTitle: "Burke Passive Fire Protection | Services",
+    pageDesc: "Our services consist of passive fire protection, penetration seals, intumescent paint, fire curtain barriers, steel encasements and fire stopping surveys.",
+    canonicalPath: "/services"
+  })
 
 })
 
 app.get('/about-us', (req, res) => {
   
-  res.render("aboutUs")
+  res.render("aboutUs", {
+    pageTitle: "Burke Passive Fire Protection | About Us",
+    pageDesc: "Learn about Burke PFP, Ireland's leading fire protection specialists",
+    canonicalPath: "/about-us"
+  })
 
 })
 
 app.get('/contact-us', (req, res) => {
 
-  res.render("contactUs")
+  res.render("contactUs", {
+    pageTitle: "Burke Passive Fire Protection | Contact Us",
+    pageDesc: "Get in touch with Burke Passive Fire Protection. Based in Rathbourne, Dublin 15. Call (01) 899 6210 or email info@burkepfp.com",
+    canonicalPath: "/contact-us"
+  })
 
 })
 
