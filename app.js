f18c6c6ce3890c3110058a72dbec7336edfbb236
kburke131 <karlburke537@gmail.com>
2026-06-08 14:35:24 +0100

    server app change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Public directory to serve static files
 app.set('view engine', 'ejs');
