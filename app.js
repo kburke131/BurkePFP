5e5bc8dc760b86d3b9d232e7f353bd59c9ace97a
kburke131 <karlburke537@gmail.com>
2026-06-10 11:34:42 +0100

    testing fix

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  if (req.headers.host === 'burkepfp.ie'){
+    return res.redirect(301, 'https://burkepfp.ie' + req.url);
+  }
+
+  next();
+})
+
 // Routes
 app.get('/', (req, res) => {
 
